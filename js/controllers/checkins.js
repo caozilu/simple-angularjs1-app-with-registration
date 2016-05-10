31040760eeabaa23ff3783c94dbd2ef4a5db9d1a
致思 <zilu.czl@alibaba-inc.com>
2016-05-10 17:15:29 +0800

    add search and order

--- js/controllers/checkins.js
+++ js/controllers/checkins.js
@@ -13,17 +13,24 @@ myApp.controller('CheckInsController', ['$scope', '$rootScope', '$location', '$f
     var checkinsList = $firebaseArray(ref);
     $scope.checkins = checkinsList;
 
+    $scope.order = 'name';
+    $scope.direction = null;
+    $scope.query = '';
+
     $scope.addCheckin = function() {
-      var checkinsInfo = $firebaseArray(ref);
-      var myData = {
+      checkinsList.$add({
         name: $scope.user.name,
         email: $scope.user.email,
         date: Firebase.ServerValue.TIMESTAMP
-      };
-
-      checkinsInfo.$add(myData).then(function(){
+      }).then(function(){
         $location.path('/checkins/' + $scope.whichuser + '/' + $scope.whichmeeting + '/checkinsList');
       });
+    };
+
+    $scope.deleteCheckin = function(id){
+      var refDel = new Firebase(FIREBASE_URL + 'users/' + $scope.whichuser + '/meetings/' + $scope.whichmeeting + '/checkins/' + id);
+      var record = $firebaseObject(refDel);
+      record.$remove(id);
     };
 
   }]);
